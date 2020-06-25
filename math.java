1c4a491400f68e9de2659ed04c14400c2e2a9381
Paulo Dutra <paulodutra.info@gmail.com>
2020-06-25 16:38:19 -0300

    alteraçao realizada para poder testar a integraçao com o github 3

--- math.java
+++ math.java
@@ -1,5 +1,5 @@
 public class math {
-    public static int soma(int numero1, int numero2){
-        return numero1 + numero2;
+    public static int soma(int nu1, int nu2){
+        return nu1 + nu2;
     }
 }
